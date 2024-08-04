6d6c1646999aacac37d581aea3317508dfdd3923
lancervs <aatomala@espol.edu.ec>
2024-08-04 09:28:45 -0500

    /build_apk

--- lib/classes/product.dart
+++ lib/classes/product.dart
@@ -223,7 +223,7 @@ Future<void> saveAndUploadProductsAsPdf(List<Product> products) async {
 /// Download the generated PDF
 Future<void> downloadPdfFile() async {
   final Uri url =
-      Uri.parse('https://servernintventario.onrender.com/download-excel/');
+      Uri.parse('https://servernintventario.onrender.com/download-pdf/');
   if (!await launchUrl(url)) {
     throw Exception('Could not launch $url');
   }
